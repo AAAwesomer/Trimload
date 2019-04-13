7178c673b872193e99777189953a446130756d72
aarni <aarni.alasaarela@gmail.com>
2019-04-13 13:25:02 +0300

    made iframe extend the full width of dialog

--- public/content.js
+++ public/content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(request => {
   if (request.type === 'getImages') {
     const modal = document.createElement('dialog');
     modal.setAttribute("style", "height:90%; width:90%");
-    modal.innerHTML = `<iframe id="imageFetcher" style="height:100%"></iframe>
+    modal.innerHTML = `<iframe id="imageFetcher" style="height:100%; width:100%"></iframe>
         <div style="position:absolute; top:0px; left:5px;">  
             <button>x</button>
         </div>`;
